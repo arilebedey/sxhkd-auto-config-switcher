a66f47e30b55609ddff698aa20b4443b8934c385
Aristarkh Lebedev <34816154+arilebedey@users.noreply.github.com>
2022-04-14 10:44:52 +0300

    Update AutoChSX.py

--- AutoChSX.py
+++ AutoChSX.py
@@ -6,11 +6,13 @@ def GetWin():
     output = check_output(['getactivewin'], stderr=DEVNULL, stdin=DEVNULL).decode("utf-8").rstrip('\n')
     return(output)
 
+# Retrieve current sxhkd configuration
 def GetSx():
     with open(r'/home/ari/.sx/status.txt', 'r') as fr:
         fl = fr.readline()
         return(fl)
 
+# Retrieve "pause" status
 def GetPause():
     with open(r'/home/ari/.sx/pause.txt', 'r') as fr:
         pause = fr.readline()
@@ -21,13 +23,12 @@ resetpause = "python3 /home/ari/Scripts/sx_undopause.py"
 
 os.system(resetpause)
 
+# Commands that trigger config switching (detected via ./MainSwitcher.py)
 tokitty = 'ionice -c 3 nice xdotool keydown control keydown alt key b; ionice -c 3 nice sleep 0.03; ionice -c 3 nice xdotool keyup control keyup alt'
 tosx = 'ionice -c 3 nice xdotool keydown control keydown alt key c; ionice -c 3 nice sleep 0.03; ionice -c 3 nice xdotool keyup control keyup alt'
 
 global lastwin
 lastwin = GetWin()
-# status = GetSx()
-# pause = GetPause()
 
 while True:
     time.sleep(0.1)
@@ -36,18 +37,14 @@ while True:
     pause = GetPause()
     # only run on window change
     if win == "kitty" and win != lastwin and status != "sx0":
-        # time.sleep(0.5)
         os.system(tokitty)
         time.sleep(0.3)
-        # print("kitty")
         lastwin = win
         continue
     if pause == "no" and win != "kitty" and status == "sx0":
         os.system(tosx)
         time.sleep(0.3)
-        # print("no kitty")
         lastwin = win
         continue
     else:
-        # print("ok")
         continue
