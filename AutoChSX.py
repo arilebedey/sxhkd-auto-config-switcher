b2539159749f97444bcb903e1b92d82548c88783
Aristarkh Lebedev <34816154+arilebedey@users.noreply.github.com>
2022-04-17 07:24:58 +0300

    Update AutoChSX.py

--- AutoChSX.py
+++ AutoChSX.py
@@ -21,47 +21,44 @@ def GetPause():
         return(pause)
 
 # For testing with https://github.com/shiro/map2
-def Hypr():
-    os.system('hyprlock2')
-    os.system('hyprlock2')
-    os.system('hyprlock2')
-
-# For testing with https://github.com/shiro/map2
 def Map2():
     output = subprocess.Popen(['map2', '-d', '/home/ari/.map2/devices.list', '/home/ari/.map2/map.m2'])
     return(output)
 
-resetpause = "python3 /home/ari/Scripts/sx_undopause.py"
+def UndoPause():
+    os.system("python3 /home/ari/Scripts/sx_undopause.py")
+    try:
+        os.system("rm /home/ari/.toggle/toggle_pause")
+    except Exception:
+        pass
 
-os.system(resetpause)
+UndoPause()
 
 # Commands that trigger config switching (detected via ./MainSwitcher.py)
 tosx0 = 'ionice -c 3 nice xdotool keydown control keydown alt key b; ionice -c 3 nice sleep 0.03; ionice -c 3 nice xdotool keyup control keyup alt'
 tosx1 = 'ionice -c 3 nice xdotool keydown control keydown alt key c; ionice -c 3 nice sleep 0.03; ionice -c 3 nice xdotool keyup control keyup alt'
+# For testing with https://github.com/shiro/map2
 map2 = 'map2 -d /home/ari/.map2/devices.list /home/ari/.map2/map.m2'
 
-global lastwin
-lastwin = GetWin()
 # Apps that require sx0 configuration
-## Script with change to sx1 configuration in all other windows
 sx0 = ["kitty", "mpv"]
+## Script will change to sx1 configuration when all other windows are focused
+
 
 while True:
     time.sleep(0.1)
     win = GetWin()
     status = GetSXConfig()
     pause = GetPause()
-    if win in sx0 and win != lastwin and status != "sx0":
+    if win in sx0 and status != "sx0":
         # Triggering config change to terminal hotkeys
         os.system(tosx0)
         time.sleep(0.15)
-        lastwin = win
         continue
-    if pause == "no" and win not in sx0 and status == "sx0" and win != lastwin:
+    if pause == "no" and win not in sx0 and status == "sx0":
         # Triggering config change to browsing hotkeys
         os.system(tosx1)
         time.sleep(0.15)
-        lastwin = win
         continue
     else:
         continue
